1e5f0554f8b3a62465d6a984026233c309d4c547
Philip Lee-Chin <phil.leechin@outlook.com>
2020-05-13 17:13:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ class App < Sinatra::Base
     params[:number].to_i.times do
       statement += params[:phrase]
     end
+    statement
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
