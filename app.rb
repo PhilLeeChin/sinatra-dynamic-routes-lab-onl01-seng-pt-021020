7c096b7995e9b971c6a90c480b9d858cef082638
Philip Lee-Chin <phil.leechin@outlook.com>
2020-05-13 17:04:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-
+    @user_name = params[:name].reverse
+    "#{@user_name}"
   end
 
   get '/square/:number' do
