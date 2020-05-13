9e7439d526930a8bbd329bb7ca3c9032ba6445b6
Philip Lee-Chin <phil.leechin@outlook.com>
2020-05-13 17:21:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     solution = params[:number1].to_i + params[:number2].to_i
-    "#{solution}"
+    "#{solution.to_s}"
   end
 
 end
